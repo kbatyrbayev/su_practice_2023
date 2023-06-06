56cdc2b2ecbd9fae89ffb42967e19dd55870c1b6
Kairat Batyrbayev <k.batyrbayev>
2023-06-06 14:30:15 +0600

    fix

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,8 +1,9 @@
 export function checkAuth() {
-  const isAuth = localStorage.getItem("isAuth");
+  return true;
+  /* const isAuth = localStorage.getItem("isAuth");
   if (isAuth) {
     location.href = "//" + window.location.host + "/pages/main/main.html";
   } else {
     location.href = "//" + window.location.host + "/pages/login/login.html";
-  }
+  } */
 }
